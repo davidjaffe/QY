07d724db906477927240a83b5089081f31c64524
David Jaffe <djaffe@bnl.gov>
2015-10-30 16:53:38 -0400

    major developments in graph utility to fill,draw graphs,hists,multigraphs

--- graphUtils.py
+++ graphUtils.py
@@ -16,10 +16,11 @@ from array import array
 class graphUtils():
     def __init__(self):
     # use in color()
-        self.goodColors = [x for x in range(1,10)]
+        self.goodColors = [1,2,3,4, 6,7,8,9] # no yellow(5) or white (0,10)
         self.goodColors.extend( [11, 12, 18] )
         self.goodColors.extend( [x for x in range(28,50)] )
         self.goodMarkers = [x for x in range(20,31) ]
+        
         return
     
     def t2dt(self,t):
@@ -66,9 +67,9 @@ class graphUtils():
         if xmi>xma:
             xmi = min(v)
             xma = max(v)
-        dx = (xma-xmi)/float(nx)
-        xmi -= dx/2.
-        xma += dx/2.
+            dx = (xma-xmi)/float(nx)
+            xmi -= dx/2.
+            xma += dx/2.
         h = TH1D(name,title,nx,xmi,xma)
         for y in v: h.Fill(y)
         return h
@@ -96,7 +97,7 @@ class graphUtils():
         canvas.Update()
         canvas.Print(pdf,'pdf')
         return
-    def drawMultiGraph(self,TMG,figdir='',SetLogy=False, abscissaIsTime = True, drawLines=True):
+    def drawMultiGraph(self,TMG,figdir='',SetLogy=False, SetLogx=False, abscissaIsTime = True, drawLines=True):
         '''
         draw TMultiGraph with legend and output as pdf
         Default is that abscissa is calendar time.
@@ -106,12 +107,10 @@ class graphUtils():
         if not TMG.GetListOfGraphs(): return  # empty
         title = TMG.GetTitle()
         name  = TMG.GetName()
+        if SetLogx: name += '_logx'
         if SetLogy: name += '_logy'
-        if debugMG: print 'graphUtils.multiGraph',title,name,'TMG.GetListOfGraphs()',TMG.GetListOfGraphs(),'TMG.GetListOfGraphs().GetSize()',TMG.GetListOfGraphs().GetSize()
+        if debugMG: print 'graphUtils.drawMultiGraph',title,name,'TMG.GetListOfGraphs()',TMG.GetListOfGraphs(),'TMG.GetListOfGraphs().GetSize()',TMG.GetListOfGraphs().GetSize()
         nGraphs = TMG.GetListOfGraphs().GetSize()
-
-        
-
 
 
         pdf = figdir + name  + '.pdf'
@@ -121,7 +120,7 @@ class graphUtils():
         if noPopUp : gROOT.ProcessLine("gROOT->SetBatch()")
         canvas = TCanvas(pdf,title,xsize,ysize)
         canvas.SetLogy(SetLogy)
-
+        canvas.SetLogx(SetLogx)
 
         # move title to left in order to put legend above plot
         gStyle.SetTitleX(0.3)
@@ -132,7 +131,7 @@ class graphUtils():
         lg = TLegend(x1,y1,x2,y2) 
         for g in TMG.GetListOfGraphs():
             t = g.GetTitle()
-            lg.AddEntry(g, t, "lp" )
+            lg.AddEntry(g, t, "LP" )
             if abscissaIsTime : self.fixTimeDisplay(g)
 
         dOption = "AP"
@@ -142,24 +141,27 @@ class graphUtils():
         if SetLogy:
             ymi,yma = 1.e20,1.e-20
             for g in TMG.GetListOfGraphs():
-                ymi = min(ymi,g.GetYaxis().GetXmin())
-                yma = max(yma,g.GetYaxis().GetXmax())
+                x,y = self.getPoints(g)
+                ymi = min(ymi,min(y))
+                yma = max(yma,max(y))
             if ymi<=0: ymi = 0.1
+            ymi = ymi/2.
+            yma = 2.*yma
             TMG.SetMinimum(ymi)
             TMG.SetMaximum(yma)
             for g in TMG.GetListOfGraphs():
                 g.SetMinimum(ymi)
                 g.SetMaximum(yma)
+                if "A" in dOption:
+                    g.SetTitle( TMG.GetTitle() )
                 g.Draw(dOption)
                 dOption = dOption.replace("A","")
-            
-
         else:
             TMG.Draw(dOption)
             if abscissaIsTime : self.fixTimeDisplay(TMG)
 
 
-        self.labelTMultiGraph(TMG)
+        self.labelTMultiGraph(TMG,debug=debugMG)
         lg.Draw()
         canvas.Draw()
         canvas.SetGrid(1)
@@ -174,6 +176,7 @@ class graphUtils():
             os.system('ps2pdf ' + ps + ' ' + pdf)
             if os.path.exists(pdf): os.system('rm ' + ps)
 
+        if debugMG: print 'graphUtils.drawMultiGraph',title,'complete'
         return
     def makeTMultiGraph(self,name,tit=None):
         title = tit
@@ -182,7 +185,7 @@ class graphUtils():
         tmg.SetName(name)
         tmg.SetTitle(title)
         return tmg
-    def labelTMultiGraph(self,tmg):
+    def labelTMultiGraph(self,tmg,debug=False):
         name = tmg.GetName()
         if 'vs' in name:
             s = name.split('_')
@@ -192,8 +195,11 @@ class graphUtils():
             xt = xt.replace('BY','/')
             yt = yt.replace('by','/')
             yt = yt.replace('BY','/')
-            tmg.GetXaxis().SetTitle(xt)
-            tmg.GetYaxis().SetTitle(yt)
+            if debug:
+                print 'graphUtils.labelTMultiGraph: xt,yt',xt,yt,'tmg',tmg
+                print 'tmg.GetXaxis()',tmg.GetXaxis(),'tmg.GetYaxis()',tmg.GetYaxis()
+            if tmg.GetXaxis(): tmg.GetXaxis().SetTitle(xt)
+            if tmg.GetYaxis(): tmg.GetYaxis().SetTitle(yt)
         return
     def makeTGraph(self,u,v,title,name,ex=None,ey=None):
         if ex is None:
@@ -216,12 +222,31 @@ class graphUtils():
         c = n%LC
         obj.SetLineColor( self.goodColors[c] )
         if debug: print 'color: obj',obj,'n',n,'obj.IsA().GetName()',obj.IsA().GetName()
-        if obj.IsA().GetName()=='TGraph':
+        oName = obj.IsA().GetName()
+        if oName=='TGraph' or oName=='TGraphErrors':
             if M==n:
                 m = M%LM
             else:
                 m = int(float(n)/float(LC))%LM
             obj.SetMarkerStyle( self.goodMarkers[m] )
             if setMarkerColor: obj.SetMarkerColor( self.goodColors[c] )
-            if debug: print 'color: m',m,'self.goodMarkers[m]',self.goodMarkers[m]
+            if debug: print 'color:',obj.GetName(),'m',m,'self.goodMarkers[m]',self.goodMarkers[m]
         return
+    def getPoints(self,g,getErrors=False):
+        '''
+        return abscissa,ordinate values of input graph g
+        also return errors if getErrors is True
+        '''
+        x,y = [],[]
+        if getErrors: dx,dy = [],[]
+        for i in range(g.GetN()):
+            a,b = ROOT.Double(0),ROOT.Double(0)
+            OK = g.GetPoint(i,a,b)
+            if OK!=-1:
+                x.append(a)
+                y.append(b)
+                if getErrors:
+                    dx.append(g.GetErrorX(i))
+                    dy.append(g.GetErrorY(i))
+        if getErrors: return x,y,dx,dy
+        return x,y
